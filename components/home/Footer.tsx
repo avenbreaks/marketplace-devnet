2dc7f68bf1283102f0672f025c5d190c1c4019fd
avenbreaks <dev-cosmware@gitshock.com>
2024-12-06 01:32:00 +0700

    initial commit

---
+++ components/home/Footer.tsx
@@ -0,0 +1,108 @@
+import { FC } from 'react'
+import { Text, Box, Flex, Anchor, Button } from '../primitives'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+type SectionTitleProps = {
+  title: string
+}
+
+const SectionTitle: FC<SectionTitleProps> = ({ title }) => (
+  <Text style="subtitle1" css={{ color: '$gray12', mb: 8 }}>
+    {title}
+  </Text>
+)
+
+type SectionLinkProps = {
+  name: string
+  href: string
+}
+
+const SectionLink: FC<SectionLinkProps> = ({ name, href }) => (
+  <Anchor
+    target="_blank"
+    rel="noopener noreferrer"
+    href={href}
+    weight="medium"
+    css={{ fontSize: 14, mt: 16 }}
+  >
+    {name}
+  </Anchor>
+)
+
+const developerSectionLinks = [
+  {
+    name: 'Docs',
+    href: 'https://docs.glitshock.ai',
+  },
+  {
+    name: 'Github',
+    href: 'https://github.com/gitshock-labs',
+  }
+]
+
+const companySectionLinks = [
+  {
+    name: 'Jobs',
+    href: 'https://glitshock-ai.com/careers',
+  },
+  {
+    name: 'DEX Terminal',
+    href: 'https://dexscreener.com/base/contracts',
+  }
+]
+
+export const Footer = () => {
+  return (
+    <Flex
+      justify="between"
+      css={{
+        borderTop: '1px solid $gray7',
+        borderStyle: 'solid',
+        p: '$5',
+        '@lg': {
+          p: '$6',
+        },
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        gap: 36,
+        '@bp600': {
+          flexDirection: 'row',
+          gap: 0,
+        },
+      }}
+    >
+      <Flex css={{ gap: 80, '@bp600': { gap: 136 } }}>
+        <Flex direction="column">
+          <SectionTitle title="Developers" />
+          {developerSectionLinks.map((props) => (
+            <SectionLink key={props.name} {...props} />
+          ))}
+        </Flex>
+        <Flex direction="column">
+          <SectionTitle title="Company" />
+          {companySectionLinks.map((props) => (
+            <SectionLink key={props.name} {...props} />
+          ))}
+        </Flex>
+      </Flex>
+      <Flex
+        direction="column"
+        css={{ alignItems: 'flex-start', '@bp600': { alignItems: 'flex-end' } }}
+      >
+        <SectionTitle title="Join Glitshock-AI Community" />
+        <Flex css={{ gap: '$4', mt: 16 }}>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/Glitshock-AI"
+          >
+            <Button size="xs" color="gray3">
+              <FontAwesomeIcon icon={faTwitter} width={14} height={14} />
+            </Button>
+          </a>
+        </Flex>
+      </Flex>
+    </Flex>
+  )
+}
